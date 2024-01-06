214a119737e3e233e4639aad67503e2baf3ae0d1
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-07 01:25:49 +0530

    style: add navbar effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,35 @@
 
 const Navbar = () => {
   return (
-    <header className="container-md mx-auto px-20 shadow">
-    <nav className="flex justify-between p-4 font-bold antialiased">
-      <h1 className="text-lg">Dpunkr.dev</h1>
-      <div className="space-x-4">
-        <a href="/">Home</a>
-        <a href="/create">About</a>
-        <a href="/about">Projects</a>
-        <a href="/about">Contact</a>
+    <header className="container-md mx-auto px-20 shadow-md ">
+    <nav className="flex justify-between py-4">
+    <div className="h-2">
+    <div className="button" data-text="Awesome">
+        <span className="actual-text">&nbsp;dpunkr&nbsp;</span>
+        <span aria-hidden="true" className="hover-text">&nbsp;dpunkr&nbsp;</span>
+    </div>
+    </div>
+      <div className="space-x-2">
+        <button>
+            <span className="box">
+                Home
+            </span>
+        </button>
+        <button>
+            <span className="box">
+                About
+            </span>
+        </button>
+        <button>
+            <span className="box">
+                Projects
+            </span>
+        </button>
+        <button>
+            <span className="box">
+                Contact
+            </span>
+        </button>
       </div>
     </nav>
     </header>
